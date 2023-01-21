237f2c7f16550889e064f4d8bcc0250d91c97b08
Santiagommvv <santiagomorales11221@gmail.com>
2023-01-21 20:03:00 -0300

    add card dinámico

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //var que apunta al HTML que genera el botón 
 const boton = document.querySelector('[data-form-btn]');
 
-//funcionalidad del botón
-boton.addEventListener('click', crearTarea);
-
 //declaración de la función que quiero pasarle al listener
 const crearTarea = (evento) => {
   evento.preventDefault();
+  
   const entrada = document.querySelector('[data-form-input]');
+  const listaTareas = document.querySelector('[data-list]');
   const valorEntrada = entrada.value;
-  entrada.value = "";
-  const tarea = document.querySelector('[data-tarea]');
-const nuevaTarea = `<div>
+    entrada.value = "";
+
+  const tarea = document.createElement('li');
+  const contenido = `<div>
                      <i class="far fa-check-square icon"></i>
                      <span class="task">${valorEntrada}</span>
                     </div>
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`
-  tarea.innerHTML = nuevaTarea;                    
- //console.log(nuevaTarea);
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+   tarea.innerHTML = contenido;                    
+   listaTareas.appendChild(tarea);
 }
 
+//funcionalidad del botón
+boton.addEventListener('click', crearTarea);
 
