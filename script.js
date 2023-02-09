432b0b5acca934556f2db7cab999f84377c5f731
Santiagommvv <santiagomorales11221@gmail.com>
2023-02-09 20:16:09 -0300

    fechas ordenadas, falta eliminacion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import {addTask} from "./componentes/addTask.js";
-import {readTasks} from "./componentes/readTasks.js";
+import {displayTasks} from "./componentes/readTasks.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
 btn.addEventListener('click', addTask);
 
-readTasks();
+displayTasks();
